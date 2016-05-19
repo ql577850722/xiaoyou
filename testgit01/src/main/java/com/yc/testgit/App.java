a4c7649e430188cd4df55f8d3ef788d5207971ca
ysq0304 <1030583546@qq.com>
2016-05-19 19:59:21 +0800

    修改了app.java代码

--- testgit01/src/main/java/com/yc/testgit/App.java
+++ testgit01/src/main/java/com/yc/testgit/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "修改的内容。。。。。。。。。。。。" );
+        System.out.println( "再一次进行修改。。" );
     }
 }
